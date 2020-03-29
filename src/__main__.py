dd2b8a6ae523a3f3abd0895abb18e296656c2c1b
Etienne Landuré <elandure@gmail.com>
2020-03-29 23:14:34 +0200

    Use HTTPExtractor as default

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,11 @@
 import pathlib
 
-from . import extractor
+from . import http
 
 if __name__ == "__main__":
     url = ("http://iksr.bafg.de/iksr/dl_zippen.asp?S=1&JA={year}&ME=Bimm,EmLo,"
            "Emme,Gori,Hage,Kamp,Kemb,KoBr,KoMo,KoRh,LaKa,Laut,Lobi,Maas,Reki,"
            "SeLa,Selz,StRh,ViNe,Vree,Vure,Weil&KG={parameter}")
     folder = pathlib.Path(r"./data")
-    extractor = extractor.HTTPExtractor(url, 1978, 2018, folder)
+    extractor = http.HTTPExtractor(url, 1978, 2018, folder)
     extractor.run()
